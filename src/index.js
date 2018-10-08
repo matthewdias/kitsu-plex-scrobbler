dd4a6f66b383ac375af6e2f75a479f1a7fcf51bb
matthewdias <matthewdias@me.com>
2018-10-07 20:53:04 -0500

    Warn when not authorized

--- src/index.js
+++ src/index.js
@@ -8,17 +8,19 @@ const plex = new Plex(process.env.PLEX_HOST)
 const tail = new Tail(untildify(process.env.PLEX_LOG))
 
 tail.on('line', async (line) => {
-  if (kitsu.authorized && plex.authorized) {
-    let matches = line.match(/Library item (\d+) \'(.*?)\' got played by account (\d+)!.*?/)
-    if (matches) {
-      let [ match, id, title, account ] = matches
-      console.log('pmslog:', match)
+  let matches = line.match(/Library item (\d+) \'(.*?)\' got played by account (\d+)!.*?/)
+  if (matches) {
+    let [ match, id, title, account ] = matches
+    console.log('pmslog:', match)
 
+    if (kitsu.authorized && plex.authorized) {
       let libraries = process.env.PLEX_LIBRARIES.split(",").map(library => library.trim())
       let metadata = await plex.getMetadata(libraries, id)
       if (metadata) {
         kitsu.scrobble(metadata)
       }
+    } else {
+      console.log('not authorized')
     }
   }
 })
