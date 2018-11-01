ecbaf6dbb8dc0e82eb9fb7a855040f80cc540a0d
matthewdias <matthewdias@me.com>
2018-10-31 19:33:38 -0500

    Add error message for bad PLEX_LOG

--- src/server/monitor.js
+++ src/server/monitor.js
@@ -8,32 +8,37 @@ const plex = require('./data/plex')
 const kitsu = require('./data/kitsu')
 const User = require('./models/User')
 
-const tail = new Tail(untildify(path.join(PLEX_LOGS, 'Plex Media Server.log')))
+try {
+  const tail = new Tail(untildify(path.join(PLEX_LOGS, 'Plex Media Server.log')))
 
-tail.on('line', async (line) => {
-  try {
-    let matches = line.match(/Library item (\d+) \'(.*?)\' got played by account (\d+)!.*?/)
-    if (matches) {
-      let [ match, key, title, id ] = matches
-      console.log('pmslog:', match)
+  tail.on('line', async (line) => {
+    try {
+      let matches = line.match(/Library item (\d+) \'(.*?)\' got played by account (\d+)!.*?/)
+      if (matches) {
+        let [ match, key, title, id ] = matches
+        console.log('pmslog:', match)
 
-      let user = await User.query().findById(id)
-      if (!user || !user.kitsuUser) {
-        return console.log('user has not logged in')
+        let user = await User.query().findById(id)
+        if (!user || !user.kitsuUser) {
+          return console.log('user has not logged in')
+        }
+
+        user = await buildUser(user)
+        let sections = user.sections
+          .filter(s => s.scrobble)
+          .map(s => s.uuid)
+
+        let metadata = await plex.getMetadata(sections, key, user.authToken)
+        if (metadata) {
+          let kitsuUser = await kitsu.getUser(user)
+          kitsu.scrobble(user, kitsuUser, metadata)
+        }
       }
+    } catch (e) { console.error(e) }
+  })
 
-      user = await buildUser(user)
-      let sections = user.sections
-        .filter(s => s.scrobble)
-        .map(s => s.uuid)
-
-      let metadata = await plex.getMetadata(sections, key, user.authToken)
-      if (metadata) {
-        let kitsuUser = await kitsu.getUser(user)
-        kitsu.scrobble(user, kitsuUser, metadata)
-      }
-    }
-  } catch (e) { console.error(e) }
-})
-
-console.log('monitoring pmslog at', PLEX_LOGS)
+  console.log('monitoring pmslog at', PLEX_LOGS)
+} catch (e) {
+  console.error('failed to monitor pmslog. check $PLEX_LOGS value')
+  process.exit(1)
+}
